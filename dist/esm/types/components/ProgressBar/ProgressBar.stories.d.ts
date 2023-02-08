53c473c1714d3b2eb9b7089bbd2321510db3760e
Liyu M <liyuumk@gmail.com>
2023-02-09 02:07:33 +0300

    publishing to npm

--- dist/esm/types/components/ProgressBar/ProgressBar.stories.d.ts
+++ dist/esm/types/components/ProgressBar/ProgressBar.stories.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { ProgressBarProps } from "./ProgressBar";
-declare const _default: ComponentMeta<({ value, completedColor, showNumber, }: ProgressBarProps) => JSX.Element>;
+declare const _default: ComponentMeta<({ value, showNumber }: ProgressBarProps) => JSX.Element>;
 export default _default;
-export declare const Primary: ComponentStory<({ value, completedColor, showNumber, }: ProgressBarProps) => JSX.Element>;
+export declare const Primary: ComponentStory<({ value, showNumber }: ProgressBarProps) => JSX.Element>;
