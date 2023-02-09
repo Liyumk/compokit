a52f509b7dcb89911889b859d53d532801d13abd
Liyu M <liyuumk@gmail.com>
2023-02-09 16:56:01 +0300

    version 0.0.4

---
+++ dist/esm/types/src/components/ModalDialog/ModalBody/ModalBody.d.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+export interface ModalBodyProps {
+    children: ReactNode;
+}
+declare const ModalBody: (props: ModalBodyProps) => JSX.Element;
+export default ModalBody;
