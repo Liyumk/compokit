c6eeec56ab10062fb14034d6f804325f8d174644
Liyu M <liyuumk@gmail.com>
2023-02-10 20:48:34 +0300

    common styles

--- src/components/GlobalStyleWrapper/GlobalStyleWrapper.tsx
+++ src/components/GlobalStyleWrapper/GlobalStyleWrapper.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { GlobalStyles } from "../../globalstyles";
+import { GlobalStyles } from "../../common/styles/globalstyles";
 
 export interface GlobalStyleWrapperProps {
     children: ReactNode;
