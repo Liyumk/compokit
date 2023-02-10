747a9c5526a1b7e2713d7a409b43c7c8d88d2c96
Liyu M <liyuumk@gmail.com>
2023-02-10 19:52:19 +0300

    code component

---
+++ src/components/Code/Code.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CodeWrapper = styled.span`
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 0.2rem;
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+    padding-top: 0.1rem;
+    padding-bottom: 0.1rem;
+`;
