27ec5aa0b2dd056241c736eda35e55e6f3162676
Liyu M <liyuumk@gmail.com>
2023-02-13 23:41:16 +0300

    button spacing

--- src/theme/defaults.ts
+++ src/theme/defaults.ts
@@ -4,13 +4,13 @@ const theme : CompoKitTheme = {
    button: {
     base: "h-9 px-2.5 rounded font-medium",
       appearance: {
-      default: "bg-[#091E420F]/[0.04] text-[#42526E] hover:bg-[#091E420F]/[0.08]",
-      danger: "bg-red-500 text-white hover:bg-red-700",
-      link: "bg-transparent text-blue-600 hover:underline hover:text-blue-500",
-      primary: 'bg-blue-600 text-white hover:bg-blue-500',
-      subtle: 'bg-transparent text-[#42526E] hover:bg-[#091E420F]/[0.05]',
-      subtleLink: 'bg-transparent text-[#42526E] hover:underline',
-      warning: 'bg-yellow-500 text-gray-900 hover:bg-yellow-600'
+      default: "bg-[#091E420F]/[0.04] text-[#42526E] hover:bg-[#091E420F]/[0.08] border border-transparent focus:ring-4 focus:ring-[#091E420F]/[0.5]",
+      danger: "bg-red-500 text-white hover:bg-red-700 border border-transparent focus:ring-4 focus:ring-red-300",
+      link: "bg-transparent text-blue-600 hover:underline hover:text-blue-500 border-transparent focus:ring-4 focus:ring-blue-300",
+      primary: 'bg-blue-600 text-white hover:bg-blue-500 border-transparent focus:ring-4 focus:ring-blue-300',
+      subtle: 'bg-transparent text-[#42526E] hover:bg-[#091E420F]/[0.05] border-transparent focus:ring-4 focus:ring-[#42526E]/[0.5]',
+      subtleLink: 'bg-transparent text-[#42526E] hover:underline border-transparent focus:ring-4 focus:ring-[#42526E]/[0.5]',
+      warning: 'bg-yellow-500 text-gray-900 hover:bg-yellow-600 border-transparent focus:ring-2 focus:ring-yellow-300',
     },
      selected: {
       default: "bg-[#091E420F]/[0.2] text-[#42526E]",
@@ -21,10 +21,10 @@ const theme : CompoKitTheme = {
       subtleLink: 'bg-transparent text-[#42526E]/[0.5] underline',
       warning: 'bg-yellow-700/[0.7]'
     },
+    disabled: "cursor-not-allowed opacity-50",
     fitContainer: 'w-full',
     icon: "w-16 h-16",
-    spacing: {compact: "gap-x-1", default: "gap-x-1.5", none: "gap-x-0"}
-
+    spacing: {compact: "h-7", default: "h-9", none: "h-auto px-0"}
   },
   progressIndicator: {
     root:{
